1d0a9a2998e48ff6a8da8e0e6b25205ab9a7c16d
Andy Cary <andy@andycary.com>
2016-09-20 19:38:17 +0100

    five phases of itil

---
+++ itil_processes.py
@@ -0,0 +1,16 @@
+# Variables include the 5 phases of the ITIL lifecycle
+
+ss = "Service Strategy"
+sd = "Service Design"
+st = "Service Transition"
+so = "Service Operation"
+ci = "Continuous Integration"
+
+print("The five stages of service management according to ITIL are:\n")
+print(ss)
+print(sd)
+print(st)
+print(so)
+print(ci)
+
+exit()
