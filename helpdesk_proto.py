6433ea8df0ab6a053ff236d03e4b02cde427e7cd
Andy Cary <andy@andycary.com>
2016-09-22 18:57:06 +0100

    update tickets

--- helpdesk_proto.py
+++ helpdesk_proto.py
@@ -1,10 +1,22 @@
 # Define help desk ticket classes
 
-class Ticket:
+class Ticket():
     """ An example ticket structure """
     status = "unresolved"
     priority = 3
+    department = "Accounting"
+
+
+
+# Create ticket
 
 x = Ticket()
+y = Ticket()
+
+
+
+
+# Print ticket
 
 print(x)
+print(y)
