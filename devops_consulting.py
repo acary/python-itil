29df5be681ef0605473fcb8336f2a51b7d1fe480
Andy Cary <andy@andycary.com>
2016-09-22 21:23:45 +0100

    update challenges

---
+++ devops_consulting.py
@@ -0,0 +1,27 @@
+# Define business attributes
+
+class Business:
+    name = "Company name"
+    industry = "Industry"
+    size = "Size"
+    revenue = 1000000
+
+# Define business challenges
+
+challenges  = []
+
+# App hosting
+challenges.append("App Cloud Hosting")
+
+# Cloud data storage
+cloud_storage = ["Amazon AWS", "Microsoft Azure"]
+challenges.append(cloud_storage)
+
+# Analytics
+analytics = ["Application Performance Monitoring", "Network Analysis", "Data Storage Analysis"]
+challenges.append(analytics)
+
+
+# Display business challenges
+
+print(challenges)
