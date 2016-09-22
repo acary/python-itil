6595f31ab243baaba50bb88b351d8b145713e1c1
Andy Cary <andy@andycary.com>
2016-09-22 18:39:55 +0100

    added ticket class

---
+++ helpdesk_proto.py
@@ -0,0 +1,10 @@
+# Define help desk ticket classes
+
+class Ticket:
+    """ An example ticket structure """
+    status = "unresolved"
+    priority = 3
+
+x = Ticket()
+
+print(x)
---
+++ process_functions.py
@@ -0,0 +1,14 @@
+# Select which process
+
+def service_strategy():
+    print(123)
+
+
+# Learn attributes of each process
+
+
+# Call function
+service_strategy()
+
+
+exit()
