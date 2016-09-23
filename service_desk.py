9df2c8d9c732d005fad31f58d5647f82fa031d1f
Andy Cary <andy@andycary.com>
2016-09-23 03:50:39 +0100

    add header

--- service_desk.py
+++ service_desk.py
@@ -3,5 +3,16 @@
 def main():
     print("Welcome to the Service Desk!")
 
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+fib(100)
+
+
+
 if __name__ == "__main__":
     main()
