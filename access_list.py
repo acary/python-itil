5f8be30e29a750531e49e17a3005e25a79ca1554
Andy Cary <andy@andycary.com>
2016-10-07 22:01:42 +0100

    created devices as dictionary

--- access_list.py
+++ access_list.py
@@ -15,10 +15,15 @@ networks = [net1, net2, net3, net4, net5]
 
 # devices
 
-phone1 = "Phone 1"
-tablet1 = "Tablet 1"
-laptop1 = "Laptop 1"
-desktop1 = "Desktop 1"
+# phone1 = "Phone 1"
+# tablet1 = "Tablet 1"
+# laptop1 = "Laptop 1"
+# desktop1 = "Desktop 1"
+
+phone = {'Make': 'Phone', 'Model': 6564, 'IP': '192.168.1.1'}
+tablet = {'Make': 'Tablet', 'Model': 6468, 'IP': '192.168.1.2'}
+laptop = {'Make': 'Laptop', 'Model': 1264, 'IP': '192.168.1.3'}
+desktop = {'Make': 'Desktop', 'Model': 6494, 'IP': '192.168.1.4'}
 
 # add and remove devices to networks
 
@@ -39,13 +44,13 @@ def drop_from_network(device, network):
     network.remove(device)
 
 # Add device to network
-add_to_network(phone1, net1)
-add_to_network(tablet1, net1)
-add_to_network(laptop1, net1)
-add_to_network(desktop1, net1)
+add_to_network(phone, net1)
+add_to_network(tablet, net1)
+add_to_network(laptop, net1)
+add_to_network(desktop, net1)
 
 # Remove device from network
-drop_from_network(phone1, net1)
+drop_from_network(phone, net1)
 
 # Display network
 print(net1)
